4c8e4d7c41d8306760511d6e03759122e71e45f4
OmarIbrahim991 <omar.ibrahim.991@gmail.com>
2020-02-15 00:28:27 +0200

    First Deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Quotes from './quotes'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class Wrapper extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quote: 'Start where you are. Use what you have. Do what you can.',
+      author: 'Arthur Ashe',
+      num: 0
+    }
+  }
+
+  getNewQuote = () => {
+    let index = Math.floor(Math.random()*Quotes.length)
+    while (index === this.state.num) {
+      index = Math.floor(Math.random()*Quotes.length)
+    }
+    this.setState({quote: Quotes[index].quote, author: Quotes[index].author, num: index})
+  }
+
+  render() {
+    return (
+    <div id="quote-box">
+      <div id="text">
+        {this.state.quote}
+      </div>
+      <div id="author">
+        -{this.state.author}
+      </div>
+      <div id="buttons">
+        <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet</a>
+        <a id="new-quote" href="#" onClick={this.getNewQuote}>New quote</a>
+      </div>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
---
+++ src/quotes.js
@@ -0,0 +1,53 @@
+const QUOTES = [
+    {
+        quote: "Start where you are. Use what you have. Do what you can.",
+        author: "Arthur Ashe"
+    }, {
+        quote: "When one door of happiness closes, another opens, but often we look so long at the closed door that we do not see the one that has been opened for us.",
+        author: "Helen Keller"
+    }, {
+        quote: "The person who says it cannot be done should not interrupt the person who is doing it.",
+        author: "Chinese Proverb"
+    }, {
+        quote: "I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.",
+        author: "Leonardo da Vinci"
+    }, {
+        quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails. Explore, Dream, Discover.",
+        author: "Mark Twain"
+    }, {
+        quote: "Whether you think you can or you think you can’t, you’re right.",
+        author: "Henry Ford"
+    }, {
+        quote: "When I stand before God at the end of my life, I would hope that I would not have a single bit of talent left and could say, I used everything you gave me.",
+        author: "Erma Bombeck"
+    }, {
+        quote: "It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live.",
+        author: "Mae Jemison"
+    }, {
+        quote: "Remember that not getting what you want is sometimes a wonderful stroke of luck.",
+        author: "Dalai Lama"
+    }, {
+        quote: "The most common way people give up their power is by thinking they don’t have any.",
+        author: "Alice Walker"
+    }, {
+        quote: "I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.",
+        author: "Michael Jordan"
+    }, {
+        quote: "Everything you’ve ever wanted is on the other side of fear.",
+        author: "George Addair"
+    }, {
+        quote: "I would rather die of passion than of boredom.",
+        author: "Vincent van Gogh"
+    }, {
+        quote: "You become what you believe.",
+        author: "Oprah Winfrey"
+    }, {
+        quote: "If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.",
+        author: "Sheryl Sandberg"
+    }, {
+        quote: "We become what we think about.",
+        author: "Earl Nightingale"
+    }
+]
+
+  export default QUOTES
