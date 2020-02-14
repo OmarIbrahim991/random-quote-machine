a28b1c1ae5663f1358cad7f70d14815c92e98e55
OmarIbrahim991 <omar.ibrahim.991@gmail.com>
2020-02-15 00:45:49 +0200

    Updates

---
+++ precache-manifest.49e9264aefba2c79d13abca05f8d16b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3698820fc817ce78111ca07db05051",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "07972ed8da78d4e84362",
+    "url": "/random-quote-machine/static/css/main.8c71d8c7.chunk.css"
+  },
+  {
+    "revision": "c5543a605ef49f917304",
+    "url": "/random-quote-machine/static/js/2.f7013b22.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/random-quote-machine/static/js/2.f7013b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07972ed8da78d4e84362",
+    "url": "/random-quote-machine/static/js/main.2c9940b4.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  }
+]);
